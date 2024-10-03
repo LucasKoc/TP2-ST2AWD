5ef8d6804f735c259397dd7c1da7da849dd4c42f
Lucas <45604254+LucasKoc@users.noreply.github.com>
2024-10-03 21:57:52 +0200

    Update webpack.config.js

--- Front/webpack.config.js
+++ Front/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
-  targets: ['web', 'es5'],
+  target: ['web', 'es5'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
